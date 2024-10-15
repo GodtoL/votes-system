b10464ff64a044bb787fc8c235cf7716cb336a2e
Elias <eliasvera793@gmail.com>
2024-10-15 15:36:06 -0300

    Se agrega las rutas para los temas

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const topicRoutes = require('./views/TopicRoutes.js')
 
 app.get('/', (req, res) => {
     res.send('Hola mundo')
 })
+
+app.use("/topic", topicRoutes)
 
 app.listen(6500, () => {
     console.log('servidor activo')
--- views/TopicRoutes.js
+++ views/TopicRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const router = express.Router() 
 
-router.get('/voting', (req, res) => {
+router.get('/', (req, res) => {
     res.json({msg: 'Consulta de temas'})
 })
 
-router.post('/voting', (req, res) => {
+router.post('/', (req, res) => {
     res.json({msg: 'Crear un tema'})
 })
+
+router.put('/', (req, res) => {
+    res.json({msg: 'Actualizar tema'})
+})
+
+router.delete('/', (req, res) => {
+    res.json({msg : "Borrar tema"})
+})
+
+module.exports = router
